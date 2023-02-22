e487af37fcf4474a727a696342c7665150a383e2
behrooz.mh <behrooz.mh@gmail.com>
2023-02-22 20:26:40 +0330

    behrooz.mh: init sample encrypt decrypt

---
+++ EncryptionTest/src/main/java/ir/behi/tools/MyDecrypt.java
@@ -0,0 +1,22 @@
+package ir.behi.tools;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class MyDecrypt {
+    public static String decrypt(String algorithm, String cipherText, SecretKey key,
+                                 IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        byte[] plainText = cipher.doFinal(Base64.getDecoder()
+                .decode(cipherText));
+        return new String(plainText);
+    }
+}
