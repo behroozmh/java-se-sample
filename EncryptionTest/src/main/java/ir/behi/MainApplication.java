99ac7c0f3bf665ea461625e2814291d0abae122b
behrooz.mh <behroozmh@gmail.com>
2023-02-23 16:48:18 +0330

    behrooz.mh: implement AES

--- EncryptionTest/src/main/java/ir/behi/MainApplication.java
+++ EncryptionTest/src/main/java/ir/behi/MainApplication.java
@@ -1,7 +1,37 @@
 package ir.behi;
 
-public class MainApplication {
-    public static void main(String[] args) {
+import ir.behi.tools.MyCrypto;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+public class MainApplication {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException,
+            InvalidAlgorithmParameterException, NoSuchPaddingException,
+            IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+
+        String text = "SakhadSecret";
+        System.out.println("## Plain First=" + text);
+
+        String password = "ThisIsSpartaThisIsSparta";
+        System.out.println("## Plain password=" + password);
+
+        String salt = "qwertyuiopasdfghjklzxcvbnm";
+        System.out.println("## Plain salt=" + salt);
+
+        String encryptText = MyCrypto.encrypt(MyCrypto.ALGORITHM, text,
+                MyCrypto.getKeyFromPassword(password, salt),
+                MyCrypto.generateIv());
+        System.out.println("## Plain encryptText=" + encryptText);
+
+        String decryptText = MyCrypto.decrypt(MyCrypto.ALGORITHM, encryptText,
+                MyCrypto.getKeyFromPassword(password, salt),
+                MyCrypto.generateIv());
+        System.out.println("## Plain decryptText=" + decryptText);
     }
 }
